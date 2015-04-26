b03a6104b8a29a6c75539775194bc744ee886dfb
amplifize server <amplifize.com@amplifize.com>
2015-04-26 20:24:37 +0400

    Change config to work with Node

--- config/boot.rb
+++ config/boot.rb
@@ -4,3 +4,5 @@ require 'rubygems'
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
 require 'bundler/setup' if File.exists?(ENV['BUNDLE_GEMFILE'])
+ENV["PATH"] = "/usr/local/bin:#{ENV['PATH']}"
+ENV["EXECJS_RUNTIME"] = 'Node'
