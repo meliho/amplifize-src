8ade4dec8b4c4dd4a13b0fd2195282ef09267d45
Melih Onvural <melih.onvural@gmail.com>
2016-04-23 23:06:37 -0400

    File for verifying certificate ownership

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,41 +1,41 @@
 class HomeController < ApplicationController
   before_filter :require_no_user, :only => :index
-  
+
   def index
     @user_session = UserSession.new
   end
-  
+
   def about
     @user_session = UserSession.new
   end
-  
+
   def contact
     @user_session = UserSession.new
   end
-  
+
   def faq
     @user_session = UserSession.new
   end
-  
+
   def team
     @user_session = UserSession.new
     render :file => 'home/about/team'
   end
-  
+
   def terms
     @user_session = UserSession.new
   end
-  
+
   def privacy
     @user_session = UserSession.new
   end
-  
+
   def why_amplifize
     @user_session = UserSession.new
     render :file => 'home/about/why'
   end
-  
-  def google_verification
+
+  def cert_verification
     render :layout => false
-  end  
+  end
 end
